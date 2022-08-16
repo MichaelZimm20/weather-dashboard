fa32d067637acab91d7d61c0ebc59d59fab55c1f
DJMIKE20 <m.zimm20@gmail.com>
2022-08-16 17:27:40 -0400

    fetch data and now displaying on page, currently hardcoded just for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //Global Variables 
 var apiKey = "&units=imperial&appid=f26d01795b1fd24a71924c250027f50c";
+var apiKey_Uv = "f26d01795b1fd24a71924c250027f50c"
 
 
 
@@ -8,15 +9,17 @@ var apiKey = "&units=imperial&appid=f26d01795b1fd24a71924c250027f50c";
 function getCurrentWeather(userSearch) {
     
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "Charlotte" + apiKey;
+    console.log(apiUrl);
     fetch(apiUrl)
     .then(function(response) {
+        //request was successful
         if (response.ok) {
             response.json().then(function(data){
                 console.log(data);
                 displayCurrentWeather(data);
                 // displayCurrentWeather(data);
             });
-        }
+        };
         
     });
     
@@ -34,13 +37,49 @@ function displayCurrentWeather (search) {
     var currentTemp = search.main.temp;
     console.log(currentTemp);
 
+    // description of weather type
+    var weatherDescription = search.weather[0].description.toUpperCase();
+    console.log(weatherDescription);
+    // description of weather
+    var weatherMainDescription = search.weather[0].main.toUpperCase();
+    console.log(weatherMainDescription);
+
     var currentWindSpeed = search.wind.speed;
     console.log(currentWindSpeed);
 
     var humidity = search.main.humidity;
     console.log(humidity);
 
-    var uvIndexUrl;
+
+    var lat = search.coord.lat;
+    console.log(lat);
+
+    var lon= search.coord.lon;
+    console.log(lon);
+    /*TODO: Add UV Index*/
+    var uvIndexUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,daily&appid=" + apiKey_Uv;
+    console.log(uvIndexUrl);
+
+    fetch(uvIndexUrl)
+        .then(function(response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function(data){
+                    console.log(data);
+                });
+            };
+        });
+        
+    var todaysDate = moment().format('MM' + '/'+ 'DD' + '/' + 'YYYY');
+    console.log(todaysDate);
+    $('#user-search-result').text(cityName + " (" + todaysDate + ")");
+    $('#temp').text("Temp: " + currentTemp + " \u00B0F");
+    $('#wind').text("Wind: " + currentWindSpeed + " MPH");
+    $('#humidity').text("Humidity: " + humidity + " %");
+    /*Find uv index API and link it*/
+    $('#UV-index').text("UV Index: " );
+    
+
 };
 
 
